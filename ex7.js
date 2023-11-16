d950d29b41a354f31f9fbba4fcfffdc2b446781f
klemaire60 <lemaire.kevin6020@gmail.com>
2023-11-16 11:26:52 +0100

    Exo 7

---
+++ ex7.js
@@ -0,0 +1,13 @@
+function addition(result, i ) {
+    result += i;
+    return result
+}
+
+var n = parseInt(prompt("Entrez une valeur"));
+var result = 0;
+
+for(var i = 0; i < n + 1; i++) {
+    result = addition(result, i);
+}
+
+alert( "La valeur est égale à " + result);
