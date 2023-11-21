48572e677b30b0ed6248061959449a09c63b1883
klemaire60 <lemaire.kevin6020@gmail.com>
2023-11-21 17:12:24 +0100

    Ex8

--- ex8.js
+++ ex8.js
@@ -1,3 +1,7 @@
-div = document.getElementById("1")
-alert("je vais modifier la div")
-div.innerHTML = ('Nouveau texte');
+var div = document.getElementById("1")
+var txt = "nouveau texte"
+function modiftext(div, txt){
+    alert("je vais modifier la div")
+    div.innerHTML = (txt);
+}
+modiftext(div, txt)
