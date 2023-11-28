285a1b5555c430de41254abfe918010a876d2d10
klemaire60 <lemaire.kevin6020@gmail.com>
2023-11-28 15:34:41 +0100

    ex10

--- ex9.js
+++ ex9.js
@@ -1,23 +1,10 @@
-/*var txt = "nouveau texte"
-
-function modifText(txt) {
-    alert("je vais modifier la div")
-    document.getElementsByName("nom").innerText = txt;
-}
-
-modifText(txt)
-*/
-
 var txt = "Nouveau texte";
 
 function modifText(txt) {
     alert("Je vais modifier les div");
     
-    var elements = document.getElementsByClassName("nom");
+    document.getElementsByClassName("nom").innerText = txt;
 
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].innerText = txt;
-    }
 }
 
 modifText(txt);
